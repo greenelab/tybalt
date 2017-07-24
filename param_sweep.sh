d60418019ebdfc7b1d423fb0aa0edaf3f3419357
gwaygenomics <gregory.way@gmail.com>
2017-07-24 10:32:41 -0400

    update relative anaconda python path

--- param_sweep.sh
+++ param_sweep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python scripts/vae_paramsweep.py --parameter_file 'config/parameter_sweep.tsv' --config_file 'config/pmacs_config.tsv' --python_path '~/.conda/envs/vae_pancancer/bin/python' --param_folder 'param_sweep'
+~/.conda/envs/vae_pancancer/bin/python scripts/vae_paramsweep.py --parameter_file 'config/parameter_sweep.tsv' --config_file 'config/pmacs_config.tsv' --python_path '~/.conda/envs/vae_pancancer/bin/python' --param_folder 'param_sweep'
 
