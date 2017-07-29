878ddbeb7fdee594354a88e763653e433273c0e1
gwaygenomics <gregory.way@gmail.com>
2017-07-29 17:01:58 -0400

    move distance script
    also replace single parens and remove coad gbm

--- scripts/viz/distance_viz.R
+++ scripts/viz/distance_viz.R
@@ -7,7 +7,7 @@ library(dplyr)
 library(ggplot2)
 library(ggjoy)
 
-get_density <- function(distance_df, label, gene = 'all') {
+get_density <- function(distance_df, label, gene = "all") {
   # Take a distance matrix, subset data according to `gene` variable and add
   # label for plotting.
   #
@@ -15,35 +15,35 @@ get_density <- function(distance_df, label, gene = 'all') {
   #         First element is a distance summary in long data format
   #         Second element is a ggplot2 object
 
-  if (gene == 'mutated') {
+  if (gene == "mutated") {
     distance_df <- distance_df %>%
       filter(sample_id %in% tp53_df$`#sample`) %>%
       select(sample_id, tp53_df$`#sample`)
     subset_sample_df <- sample_acronym_df %>%
       filter(sample_id %in% tp53_df$`#sample`)
 
-    gene_label <- 'TP53 mutated'
+    gene_label <- "TP53 mutated"
   } 
 
-  if (gene == 'wild-type') {
+  if (gene == "wild-type") {
     distance_df <- distance_df %>% filter(!(sample_id %in% tp53_df$`#sample`))
     distance_df <- distance_df %>% select(sample_id, distance_df$sample_id)
     subset_sample_df <- sample_acronym_df %>%
       filter(!(sample_id %in% tp53_df$`#sample`))
 
-    gene_label <- 'wild-type'
+    gene_label <- "wild-type"
   }
 
-  if (gene %in% c('all', 'wt_mut')) {
+  if (gene %in% c("all", "wt_mut")) {
     subset_sample_df <- sample_acronym_df
 
-    gene_label <- 'all'
+    gene_label <- "all"
   }
 
   # Only take the upper triangle to avoid duplicate observations
   upper_mat <- reshape2::melt(upper.tri(distance_df))
   dist_long <- reshape2::melt(distance_df)[upper_mat$value, ]
-  colnames(dist_long) <- c('sample_id', 'other_sample_id', 'distance')
+  colnames(dist_long) <- c("sample_id", "other_sample_id", "distance")
 
   # Get tissue type comparison
   dist_long$column_a_acronym <- 
@@ -56,9 +56,9 @@ get_density <- function(distance_df, label, gene = 'all') {
   dist_long <- dist_long[complete.cases(dist_long), ]
   dist_long$comparison <- paste(dist_long$column_b_acronym,
                                 dist_long$column_a_acronym,
-                                sep = '_')
+                                sep = "_")
   dist_long$comparison <- dist_long$comparison  %>%
-    dplyr::recode('LUAD_BLCA' = 'BLCA_LUAD')
+    dplyr::recode("LUAD_BLCA" = "BLCA_LUAD")
   
   # Get TP53 mutation comparison
   dist_long$column_a_status <-
@@ -69,13 +69,13 @@ get_density <- function(distance_df, label, gene = 'all') {
                                   subset_sample_df$sample_id)]
   dist_long$mutation <- paste(dist_long$column_a_status,
                               dist_long$column_b_status,
-                              sep = '_')
+                              sep = "_")
   dist_long$mutation <- dist_long$mutation  %>%
-    dplyr::recode('MUT_WT' = 'WT_MUT')
+    dplyr::recode("MUT_WT" = "WT_MUT")
   
-  if (gene == 'wt_mut') {
-    dist_long <- dist_long %>% dplyr::filter(mutation == 'WT_MUT')
-    gene_label = 'wild-type vs. mut'
+  if (gene == "wt_mut") {
+    dist_long <- dist_long %>% dplyr::filter(mutation == "WT_MUT")
+    gene_label = "wild-type vs. mut"
   }
   
   dist_long$label <- label
@@ -117,42 +117,42 @@ sample_acronym_df$mutate[match(tp53_df$`#sample`,
 # Observe density plots of distances across BLCA and LUAD
 ####################################
 # 1) All comparison
-raw_data_list <- get_density(raw_dist_df, label = 'untransformed')
+raw_data_list <- get_density(raw_dist_df, label = "untransformed")
 raw_sub_data_list <- get_density(raw_dist_subtract_df,
-                                 label = 'untransformed sub.')
-encoded_data_list <- get_density(encoded_dist_df, label = 'vae encoded')
+                                 label = "untransformed sub.")
+encoded_data_list <- get_density(encoded_dist_df, label = "vae encoded")
 encoded_sub_data_list <- get_density(encoded_sub_dist_df,
-                                     label = 'vae subtracted')
+                                     label = "vae subtracted")
 
 # 2) All TP53 mutations
 p53_raw_data_list <- get_density(raw_dist_df, gene = "mutated",
-                                 label = 'untransformed')
+                                 label = "untransformed")
 p53_raw_sub_data_list <- get_density(raw_dist_subtract_df, gene = "mutated",
-                                     label = 'untransformed sub.')
+                                     label = "untransformed sub.")
 p53_encoded_data_list <- get_density(encoded_dist_df, gene = "mutated",
-                                     label = 'vae encoded')
+                                     label = "vae encoded")
 p53_encoded_sub_data_list <- get_density(encoded_sub_dist_df, gene = "mutated",
-                                         label = 'vae subtracted')
+                                         label = "vae subtracted")
 
 # 3) All wild-type samples
 wt_raw_data_list <- get_density(raw_dist_df, gene = "wild-type",
-                                label = 'untransformed')
+                                label = "untransformed")
 wt_raw_sub_data_list <- get_density(raw_dist_subtract_df, gene = "wild-type",
-                                    label = 'untransformed sub.')
+                                    label = "untransformed sub.")
 wt_encoded_data_list <- get_density(encoded_dist_df, gene = "wild-type",
-                                    label = 'vae encoded')
+                                    label = "vae encoded")
 wt_encoded_sub_data_list <- get_density(encoded_sub_dist_df, gene = "wild-type",
-                                        label = 'vae subtracted')
+                                        label = "vae subtracted")
 
 # 4) Mutated vs. wild-type samples
 wt_mut_raw_data_list <- get_density(raw_dist_df, gene = "wt_mut",
-                                    label = 'untransformed')
+                                    label = "untransformed")
 wt_mut_raw_sub_data_list <- get_density(raw_dist_subtract_df, gene = "wt_mut",
-                                        label = 'untransformed sub.')
+                                        label = "untransformed sub.")
 wt_mut_enc_data_list <- get_density(encoded_dist_df, gene = "wt_mut",
-                                    label = 'vae encoded')
+                                    label = "vae encoded")
 wt_mut_enc_sub_data_list <- get_density(encoded_sub_dist_df, gene = "wt_mut",
-                                        label = 'vae subtracted')
+                                        label = "vae subtracted")
 
 full_df <- dplyr::bind_rows(raw_data_list[[1]],
                             raw_sub_data_list[[1]],
@@ -172,8 +172,8 @@ full_df <- dplyr::bind_rows(raw_data_list[[1]],
                             wt_mut_enc_sub_data_list[[1]])
 
 full_df$gene_label <- factor(full_df$gene_label,
-                             levels = c('all', 'wild-type', 'TP53 mutated',
-                                        'wild-type vs. mut'))
+                             levels = c("all", "wild-type", "TP53 mutated",
+                                        "wild-type vs. mut"))
 
 distance_plot_file <- file.path("figures", "tp53_distance_analysis.png")
 ggplot(full_df, aes(x = distance, y = comparison, fill = comparison)) +
@@ -185,42 +185,16 @@ ggsave(distance_plot_file, height = 5, width = 6)
 
 
 blca_luad_vaesub_mut <- p53_encoded_sub_data_list[[1]] %>%
-  dplyr::filter(comparison == 'BLCA_LUAD') %>% dplyr::select(distance)
+  dplyr::filter(comparison == "BLCA_LUAD") %>% dplyr::select(distance)
 blca_luad_vaesub_wtmut <- wt_mut_enc_sub_data_list[[1]] %>%
-  dplyr::filter(comparison == 'BLCA_LUAD') %>% dplyr::select(distance)
+  dplyr::filter(comparison == "BLCA_LUAD") %>% dplyr::select(distance)
 
 blca_luad_rawsub_mut <- p53_raw_sub_data_list[[1]] %>%
-  dplyr::filter(comparison == 'BLCA_LUAD') %>% dplyr::select(distance)
+  dplyr::filter(comparison == "BLCA_LUAD") %>% dplyr::select(distance)
 blca_luad_rawsub_wtmut <- wt_mut_raw_sub_data_list[[1]] %>%
-  dplyr::filter(comparison == 'BLCA_LUAD') %>% dplyr::select(distance)
+  dplyr::filter(comparison == "BLCA_LUAD") %>% dplyr::select(distance)
   
 
 t.test(blca_luad_vaesub_mut, blca_luad_vaesub_wtmut)
 t.test(blca_luad_rawsub_mut, blca_luad_rawsub_wtmut)
-
-
-
-gbm_coad_vaesub_mut <- ras_encoded_sub_data_list[[1]] %>%
-  dplyr::filter(comparison == 'COAD_GBM') %>% dplyr::select(distance)
-gbm_coad_vaesub_wtmut <- wt_mut_enc_sub_data_list[[1]] %>%
-  dplyr::filter(comparison == 'COAD_GBM') %>% dplyr::select(distance)
-
-gbm_coad_rawsub_mut <- ras_raw_sub_data_list[[1]] %>%
-  dplyr::filter(comparison == 'COAD_GBM') %>% dplyr::select(distance)
-gbm_coad_rawsub_wtmut <- wt_mut_raw_sub_data_list[[1]] %>%
-  dplyr::filter(comparison == 'COAD_GBM') %>% dplyr::select(distance)
-
-t.test(gbm_coad_vaesub_mut, gbm_coad_vaesub_wtmut)
-t.test(gbm_coad_rawsub_mut, gbm_coad_rawsub_wtmut)
-
-
-
-
-
-
-
-
-
-
-
 
