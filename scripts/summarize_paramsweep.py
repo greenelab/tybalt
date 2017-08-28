1a2ce2d66e12d3808bf0ff0b446274f9d6185756
gwaygenomics <gregory.way@gmail.com>
2017-08-28 11:32:31 -0400

    modify summarize parameter sweep script to custom

--- scripts/summarize_paramsweep.py
+++ scripts/summarize_paramsweep.py
@@ -12,13 +12,27 @@ Large dataframe to use in downstream visualizations
 """
 
 import os
+import argparse
 import pandas as pd
 
-param_files = os.listdir('param_sweep')
-df_list = [pd.read_table(os.path.join('param_sweep', x)) for x in param_files]
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', '--results_directory',
+                    help='location where all results are stored',
+                    default='param_sweep')
+parser.add_argument('-f', '--output_filename',
+                    help='The name of the file to store results',
+                    default='parameter_sweep_full_results.tsv')
+args = parser.parse_args()
+
+# Set hyper parameters
+results_dir = args.results_directory
+output_filename = args.output_filename
+
+param_files = os.listdir(results_dir)
+df_list = [pd.read_table(os.path.join(results_dir, x)) for x in param_files]
 
 param_df = pd.concat(df_list, axis=0)
 param_df.rename(columns={'Unnamed: 0': 'train_epoch'}, inplace=True)
 
-full_result_file = os.path.join('results', 'parameter_sweep_full_results.tsv')
+full_result_file = os.path.join('results', output_filename)
 param_df.to_csv(full_result_file, index=False, sep='\t')
