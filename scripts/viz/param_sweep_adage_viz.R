94d8c9b7de866937560a429b746438fd5bc62702
gwaygenomics <gregory.way@gmail.com>
2017-08-30 09:21:20 -0400

    update adage parameter sweep plotting

--- scripts/viz/param_sweep_adage_viz.R
+++ scripts/viz/param_sweep_adage_viz.R
@@ -18,7 +18,7 @@ param_df <- readr::read_tsv(param_file,
 param_melt_df <- reshape2::melt(param_df,
                                 id.vars = c("learning_rate", "batch_size",
                                             "epochs", "sparsity", "noise",
-                                            "train_epoch", "zero_nodes"),
+                                            "train_epoch"),
                                 measure.vars = c("loss", "val_loss"),
                                 variable.name = "loss_type",
                                 value.name = "loss")
@@ -50,8 +50,7 @@ param_melt_df$learning_rate <-
                        `1.0` = "lr: 1.0",
                        `1.1` = "lr: 1.1")
 
-
-# Order and recode kappa variables
+# Order and recode sparsity variables
 param_melt_df$sparsity <- factor(param_melt_df$sparsity,
                                  levels = c("0.0", "0.001", "1e-06"))
 param_melt_df$sparsity <-
@@ -60,7 +59,7 @@ param_melt_df$sparsity <-
                        `1e-06` = "sparsity: 1e-6",
                        `0.001` = "sparsity: 0.001")
 
-# Order and recode kappa variables
+# Order and recode noise variables
 param_melt_df$noise <- factor(param_melt_df$noise,
                                  levels = c("0.0", "0.05", "0.1", "0.5"))
 param_melt_df$noise <-
@@ -70,7 +69,7 @@ param_melt_df$noise <-
                        `0.1` = "noise: 0.1",
                        `0.5` = "noise: 0.5")
 
-# Sweep over measurements of kappa with batch_size and epochs as facets
+# Sweep over noise and sparsity with batch_size and epochs as facets
 for (n in unique(param_melt_df$noise)) {
   subset_param <- param_melt_df %>% dplyr::filter(noise == n)
   
@@ -87,7 +86,7 @@ for (n in unique(param_melt_df$noise)) {
       ggtitle(paste("noise =", n, ", sparsity =", s))
     
     output_fig <- file.path("figures", "param_sweep", "adage", "full_param_")
-    output_fig <- paste0(output_fig, n, "_noise_", s, "_sparsity.png")
+    output_fig <- paste0(output_fig, n, "noise_", s, "sparsity.png")
     ggsave(output_fig, plot = p, height = 5, width = 6)
   }
 }
@@ -95,48 +94,29 @@ for (n in unique(param_melt_df$noise)) {
 # Plot the final loss at training end
 final_df <- param_melt_df %>% dplyr::filter(loss_type == "val_loss")
 final_df <- final_df %>%
-  dplyr::group_by(learning_rate, batch_size, epochs, noise, sparsity,
-                  zero_nodes) %>%
+  dplyr::group_by(learning_rate, batch_size, epochs, noise, sparsity) %>%
   dplyr::summarize(end_loss = tail(loss, 1))
-final_df <- final_df[order(as.numeric(final_df$zero_nodes)), ]
 
-p <- ggplot(final_df, aes(x = learning_rate, y = end_loss)) +
-  geom_point(aes(color = sparsity, shape = epochs), size = 1) +
+# High sparsity did not train at all, remove this parameter combination
+final_df <- final_df %>%
+  dplyr::filter(sparsity != 'sparsity: 0.001')
+
+# For plotting purposes, recode the epochs variable again
+final_df$epochs <- 
+  dplyr::recode_factor(final_df$epochs, 
+                       "epochs: 40" = "40", 
+                       "epochs: 80" = "80",
+                       "epochs: 100" = "100")
+
+# Plot final training parameters
+p <- ggplot(final_df, aes(x = epochs, y = end_loss)) +
+  geom_point(aes(color = learning_rate, shape = sparsity), size = 1) +
   facet_grid(batch_size ~ noise, scales = "free") +
   theme_bw() +
-  ylab("Validation Loss at Training End") + xlab("") +
-  theme(axis.text = element_text(size = rel(1.2)),
-        axis.text.x = element_text(angle = 45, hjust = 1),
-        legend.text = element_text(size = rel(1.1)))
+  ylab("Validation Loss at Training End") + 
+  xlab("Epochs") +
+  theme(legend.text = element_text(size = rel(1.1)))
 
 output_sweep <- file.path("figures", "param_sweep", "adage",
                           "final_param_val_loss.png")
-ggsave(output_sweep, plot = p, height = 7, width = 8)
-
-# Incorporate the number of non-zero nodes into selection
-p <- ggplot(final_df, aes(x = end_loss, y = as.numeric(zero_nodes))) +
-  facet_grid(batch_size~sparsity) + 
-  xlab("Validation Loss") +
-  ylab("Number of Zero Nodes") + 
-  geom_point(aes(color = learning_rate, shape = epochs, size = noise)) +
-  theme_bw()
-
-output_sweep <- file.path("figures", "param_sweep", "adage",
-                          "final_param_val_loss_zero_nodes.png")
-ggsave(output_sweep, plot = p, height = 7, width = 8)
-
-# What about number of non-zero nodes for all models with < 0.025 val loss?
-# Replot this condensed figure
-good_models <- final_df %>% dplyr::filter(end_loss < 0.025)
-good_models <- good_models[order(good_models$zero_nodes), ]
-
-p <- ggplot(good_models, aes(x = end_loss, y = as.numeric(zero_nodes))) +
-  facet_grid(batch_size~sparsity) + 
-  xlab("Validation Loss") +
-  ylab("Number of Zero Nodes") + 
-  geom_point(aes(color = learning_rate, shape = epochs, size = noise)) +
-  theme_bw()
-
-output_sweep <- file.path("figures", "param_sweep", "adage",
-                          "final_param_goodmodels_loss_zero_nodes.png")
-ggsave(output_sweep, plot = p, height = 7, width = 8)
+ggsave(output_sweep, plot = p, height = 6, width = 8)
