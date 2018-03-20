bcd49535452997e60433cdfd1e214534fd454c28
Greg Way <gregory.way@gmail.com>
2018-03-20 12:46:03 -0400

    Adding ADAGE model to tsne visualization (#114)
    * add adage tsne plot to script
    
    * add tsne adage data generation
    
    also nbconverted script
    
    * update pdfs
    
    * add color_df to plotting function
    
    * add library(readr)

--- scripts/nbconverted/tsne_tybalt_features.py
+++ scripts/nbconverted/tsne_tybalt_features.py
@@ -9,12 +9,14 @@
 
 # In[1]:
 
+
 import os
 import pandas as pd
 from sklearn import manifold
 
 
 # In[2]:
+
 
 # Load VAE feature activations per sample
 encoded_file = os.path.join('data', 'encoded_rnaseq_onehidden_warmup_batchnorm.tsv')
@@ -24,6 +26,16 @@ encoded_df.head(2)
 
 # In[3]:
 
+
+# Load ADAGE feature activations per sample
+adage_file = os.path.join('data', 'encoded_adage_features.tsv')
+adage_df = pd.read_table(adage_file, index_col=0)
+adage_df.head(2)
+
+
+# In[4]:
+
+
 # Load Zero-One transformed (min-max scaled) RNAseq data
 rnaseq_file = os.path.join('data', 'pancan_scaled_zeroone_rnaseq.tsv.gz')
 rnaseq_df = pd.read_table(rnaseq_file, index_col=0)
@@ -31,7 +43,8 @@ print(rnaseq_df.shape)
 rnaseq_df.head(2)
 
 
-# In[4]:
+# In[5]:
+
 
 # Perform t-SNE on VAE encoded_features
 tsne = manifold.TSNE(n_components=2, init='pca', random_state=0, perplexity=20,
@@ -45,16 +58,32 @@ tsne_out.to_csv(tsne_out_file, sep='\t')
 tsne_out.head(2)
 
 
-# In[5]:
+# In[6]:
+
+
+# Perform t-SNE on ADAGE encoded_features
+tsne_adage = manifold.TSNE(n_components=2, init='pca', random_state=0, perplexity=20,
+                           learning_rate=300, n_iter=400)
+tsne_adage_out = tsne_adage.fit_transform(adage_df)
+tsne_adage_out = pd.DataFrame(tsne_adage_out, columns=['1', '2'])
+tsne_adage_out.index = adage_df.index
+tsne_adage_out.index.name = 'tcga_id'
+tsne_adage_out_file = os.path.join('results', 'adage_tsne_features.tsv')
+tsne_adage_out.to_csv(tsne_adage_out_file, sep='\t')
+tsne_adage_out.head(2)
+
+
+# In[7]:
+
 
 # Perform t-SNE on zero-one RNAseq features
-tsne = manifold.TSNE(n_components=2, init='pca', random_state=0, perplexity=20,
-                     learning_rate=300, n_iter=400)
-tsne_out = tsne.fit_transform(rnaseq_df)
-tsne_out = pd.DataFrame(tsne_out, columns=['1', '2'])
-tsne_out.index = rnaseq_df.index
-tsne_out.index.name = 'tcga_id'
-tsne_out_file = os.path.join('results', 'rnaseq_tsne_features.tsv')
-tsne_out.to_csv(tsne_out_file, sep='\t')
-tsne_out.head(2)
+tsne_rna = manifold.TSNE(n_components=2, init='pca', random_state=0, perplexity=20,
+                         learning_rate=300, n_iter=400)
+tsne_rna_out = tsne_rna.fit_transform(rnaseq_df)
+tsne_rna_out = pd.DataFrame(tsne_rna_out, columns=['1', '2'])
+tsne_rna_out.index = rnaseq_df.index
+tsne_rna_out.index.name = 'tcga_id'
+tsne_rna_out_file = os.path.join('results', 'rnaseq_tsne_features.tsv')
+tsne_rna_out.to_csv(tsne_rna_out_file, sep='\t')
+tsne_rna_out.head(2)
 
--- scripts/viz/tsne_viz.R
+++ scripts/viz/tsne_viz.R
@@ -1,10 +1,29 @@
 # Pan-Cancer Variational Autoencoder
 # Gregory Way 2017
 #
-# Visualize t-SNE plots of VAE and raw RNAseq data features
+# Visualize t-SNE plots of VAE, ADAGE, and raw RNAseq data features
 
 library(dplyr)
 library(ggplot2)
+library(readr)
+
+plot_tsne <- function(tsne_df, color_df) {
+  # Function to output tsne visualizations colored by cancer-type for the given
+  # input data (compressed by either Tybalt or ADAGE or raw RNAseq data)
+  p <- ggplot(tsne_df, aes(x = `1`, y = `2`, color = acronym)) +
+    geom_point(size = 0.001) +
+    scale_colour_manual(limits = color_df$`Study Abbreviation`,
+                        values = color_df$`Hex Colors`,
+                        na.value = "black",
+                        breaks = palette_order) +
+    theme_classic() +
+    theme(legend.title = element_text(size = 8),
+          legend.text = element_text(size = 5),
+          legend.key.size = unit(0.5, "line")) +
+    guides(colour = guide_legend(override.aes = list(size = 0.1),
+                                 title = 'Cancer-Type'))
+  return(p)
+}
 
 # Load encodings file with matched clinical data to subset
 clinical_file <- file.path("data", "tybalt_features_with_clinical.tsv")
@@ -13,12 +32,16 @@ clinical_df <- clinical_df %>% dplyr::select(sample_id, acronym)
 
 # Load tsne data
 tsne_vae_file <- file.path("results", "tybalt_tsne_features.tsv")
+tsne_adage_file <- file.path("results", "adage_tsne_features.tsv")
 tsne_rnaseq_file <- file.path("results", "rnaseq_tsne_features.tsv")
 
 tsne_vae_df <- readr::read_tsv(tsne_vae_file)
+tsne_adage_df <- readr::read_tsv(tsne_adage_file)
 tsne_rnaseq_df <- readr::read_tsv(tsne_rnaseq_file)
 
 tsne_vae_df <- dplyr::inner_join(tsne_vae_df, clinical_df,
+                                 by = c("tcga_id" = "sample_id"))
+tsne_adage_df <- dplyr::inner_join(tsne_adage_df, clinical_df,
                                  by = c("tcga_id" = "sample_id"))
 tsne_rnaseq_df <- dplyr::inner_join(tsne_rnaseq_df, clinical_df,
                                     by = c("tcga_id" = "sample_id"))
@@ -36,37 +59,16 @@ palette_order <- c("BRCA", "PRAD", "TGCT", "KICH", "KIRP", "KIRC", "BLCA",
 # Plot and save VAE tsne
 vae_tsne_pdf_out_file <- file.path("figures", "tsne_vae.pdf")
 vae_tsne_png_out_file <- file.path("figures", "tsne_vae.png")
-
-p <- ggplot(tsne_vae_df, aes(x = `1`, y = `2`, color = acronym)) +
-  geom_point(size = 0.001) +
-  scale_colour_manual(limits = tcga_colors$`Study Abbreviation`,
-                      values = tcga_colors$`Hex Colors`,
-                      na.value = "black",
-                      breaks = palette_order) +
-  theme_classic() +
-  theme(legend.title = element_text(size = 8),
-        legend.text = element_text(size = 5),
-        legend.key.size = unit(0.5, "line")) +
-  guides(colour = guide_legend(override.aes = list(size = 0.1),
-                               title = 'Cancer-Type'))
-
+p <- plot_tsne(tsne_vae_df, tcga_colors)
 ggsave(vae_tsne_pdf_out_file, plot = p, width = 6, height = 4.5)
 ggsave(vae_tsne_png_out_file, plot = p, width = 4, height = 3)  # PNG for repo
 
+# Plot and save ADAGE tsne
+adage_tsne_pdf_out_file <- file.path("figures", "tsne_adage.pdf")
+p <- plot_tsne(tsne_adage_df, tcga_colors)
+ggsave(adage_tsne_pdf_out_file, plot = p, width = 6, height = 4.5)
+
 # Plot and save RNAseq tsne
 rnaseq_tsne_out_file <- file.path("figures", "tsne_rnaseq.pdf")
-
-p <- ggplot(tsne_rnaseq_df, aes(x = `1`, y = `2`, color = acronym)) +
-  geom_point(size = 0.001) +
-  scale_colour_manual(limits = tcga_colors$`Study Abbreviation`,
-                      values = tcga_colors$`Hex Colors`,
-                      na.value = "black",
-                      breaks = palette_order) +
-  theme_classic() +
-  theme(legend.title = element_text(size = 8),
-        legend.text = element_text(size = 5),
-        legend.key.size = unit(0.5, "line")) +
-  guides(colour = guide_legend(override.aes = list(size = 0.1),
-                               title = 'Cancer-Type'))
-
+p <- plot_tsne(tsne_rnaseq_df, tcga_colors)
 ggsave(rnaseq_tsne_out_file, plot = p, width = 6, height = 4.5)
